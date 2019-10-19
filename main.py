b1251947e6e964dce7dd89e13aabba1d8c537607
wpucilowski <wpucilowski@nfreewave.com>
2019-10-19 10:19:16 -0600

    Lesson05 assignment

--- main.py
+++ main.py
@@ -17,9 +17,27 @@ def get_fact():
     return facts[0].getText()
 
 
+def post_fact():
+
+    url = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+    text = get_fact()
+    payload = {'input_text': text}
+    resp = requests.post(url, data=payload, allow_redirects=False)
+    if resp.status_code == 302:
+        try:
+            return resp.headers['Location']
+        except KeyError as e:
+            raise NameError
+    else:
+        return ''
+
 @app.route('/')
 def home():
-    return "FILL ME!"
+    try:
+        url = post_fact()
+    except NameError:
+        url = "Not found"
+    return url
 
 
 if __name__ == "__main__":
